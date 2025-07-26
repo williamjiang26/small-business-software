fc77c026bfa2184736a0a92f080b35cabf995055
William Jiang <wj2065@nyu.edu>
2025-07-25 22:20:51 -0400

    landing page outline

--- client/src/app/(nondashboard)/landing/HeroSection.tsx
+++ client/src/app/(nondashboard)/landing/HeroSection.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
 import { motion } from "motion/react";
 import { Button } from "@/components/ui/button";
-import  HeroPhoto  from "../../../assets/door2.JPG";
+import HeroPhoto from "../../../assets/tdcdashboard.png";
 
 const HeroSection = () => {
   return (
-    <div className="relative h-screen">
+    <div className="relative h-screen w-screen">
       <Image
         src={HeroPhoto}
         alt="Rental Platform Hero Section"
@@ -13,31 +13,33 @@ const HeroSection = () => {
         className="object-cover object-center"
         priority
       />
+
       <div className="absolute top-0 left-0 right-0 bottom-0 bg-black/60" />
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.8 }}
-        className="absolute top-1/2 left-1/2 transform -translate-y-1/2 -translate-x-1/2 text-center"
-      >
-        <div>
-          <h1 className="text-5xl font-bold text-white mb-4">
-            Start your journey to finding the perfect place to call home.
-          </h1>
-          <p className="text-xl text-white mb-8">
-            Explore our wide range of properties to fit your lifestyle and
-            needs!
-          </p>
-          <div className="flex justify-center">
+
+      <div className="absolute left-1/2 top-1/3 transform -translate-x-1/2">
+        <motion.div
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+          className="text-center"
+        >
+          <div className="">
+            <h1 className="text-5xl font-bold text-white mb-4">
+              Tools to <span className="underline">Manage</span> Your Small Business Operations
+            </h1>
+            <p className="text-xl text-white mb-8">
+              Reliable tools and systems that you can trust and successfully
+              operate any business
+            </p>
             <Button
               onClick={() => {}}
-              className="bg-secondary-500 text-white rounded-none rounded-r-xl border-none hover:bg-secondary-600 h-12"
+              className="bg-secondary-500 text-white rounded-xl border-2 hover:bg-white hover:text-black h-12"
             >
-              Book Demo
+              Book a Demo
             </Button>
           </div>
-        </div>
-      </motion.div>
+        </motion.div>
+      </div>
     </div>
   );
 };
