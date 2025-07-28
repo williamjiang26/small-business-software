93663017bc0b7280d30c7b7a0ef3510b9313ed4b
William Jiang <wj2065@nyu.edu>
2025-07-28 11:42:17 -0400

    project direction

--- README.md
+++ README.md
@@ -1,26 +1,21 @@
-## 🚀 Why This Exists
+## The Problem
+Managing a business is difficult. There are a lot of parts to running a successful business, from operations to finances to sales. It is difficult to manage each department and determine what are success metrics for each. Additionally there are repetitive processes to fulfilling each customer order where attention to detail and organization is crucial. 
 
-this is a project i built to reliably store and organize customer orders and inventory in a small business. typical a small businesses is not technology heavy driven. the tools are out there but there is a big barrier between non-technical business owners and the integration and discovery of these tools that exist. Tools like these require a paywall or free trial and each tool requires time to learn its unique workflow. Not all businesses have the time to invest on learning a new platform or test out enough platforms to find the one that suits there needs.
 
-this project places emphasis on user experience and design and the goal is to make managing your small business as easy as scrolling tiktok.
+## Jobs to be done
+organize products in inventory, product location, types and sizes, stock levels, re-stocking info
+update customer order details, track customer orders
+identify our margins, month to month sales, revenue and profitability, costs 
 
-Our business model is designed to put your business first. Inspired by the robust tools at Google (maps, calendar, drive) we are prioritizing deep integration and logical experiences over all else. To small business' a paywall is the biggest hurdle to adopting your product so we are putting helping your business first by making the tool free to use.
 
-this tool is one of many for small business tools. some of which will include books, supply chain management, customer case management and more.
+## Product Solution
+a organization tool to manage every aspect of your business
 
-a set of tools for small businesses which are deeply integrated, tightly coupled and logically engineered.
-
-## Product Roadmap
+AI features 5. invoice generation and data population (OCR) 6. low stock notification 7. customer pre and after sales support agent
+## Engineering Roadmap
 
 1. store, edit, delete inventory products, orders and customer info
 2. design cards and detail pages
 3. store, edit and delete secondary products in details page
 4. add ui components for visualization, ui design
-
----
-
-AI features 5. invoice generation and data population (OCR) 6. low stock notification 7. customer pre and after sales support agent
-
-## Current Todos:
-
-1.
+## Current Todo: 
--- client/src/app/Components/Navbar.tsx
+++ client/src/app/Components/Navbar.tsx
@@ -1,7 +1,141 @@
 "use client";
+import Link from "../../../node_modules/next/link";
+import Image from "../../../node_modules/next/image";
+import { Button } from "@/components/ui/button";
+import LOGO from "../../assets/TDClogo.png"
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const isDashboardPage = false;
+  const authUser = false;
+  return (
+    <div
+      className=" fixed bg-opacity-10 top-0 left-0 w-full z-50 shadow-xl"
+      style={{ height: `${60}px` }}
+    >
+      <div className="flex justify-between items-center w-full py-3 px-8  text-white">
+        <div className="flex items-center gap-4 md:gap-6">
+          {isDashboardPage && (
+            <div className="md:hidden">
+              <SidebarTrigger />
+            </div>
+          )}
+          <Link
+            href="/"
+            className="cursor-pointer hover:!text-primary-300"
+            scroll={false}
+          >
+            <div className="flex items-center gap-3">
+              <Image
+                src={LOGO}
+                alt="Rentiful Logo"
+                width={24}
+                height={24}
+                className="w-6 h-6"
+              />
+              <div className="text-xl font-bold">
+                TDC
+           
+              </div>
+            </div>
+          </Link>
+          {isDashboardPage && authUser && (
+            <Button
+              variant="secondary"
+              className="md:ml-4 bg-primary-50 text-primary-700 hover:bg-secondary-500 font-light hover:!text-primary-300"
+              onClick={() => {}}
+            >
+             dd
+            </Button>
+          )}
+        </div>
+        {!isDashboardPage && (
+          <p className="text-primary-200 hidden md:block">
+
+          </p>
+        )}
+        <div className="flex items-center gap-5">
+          {authUser ? (
+            <>
+              <div className="relative hidden md:block">
+                <MessageCircle className="w-6 h-6 cursor-pointer text-primary-200 hover:text-primary-400" />
+                <span className="absolute top-0 right-0 w-2 h-2 bg-secondary-700 rounded-full"></span>
+              </div>
+              <div className="relative hidden md:block">
+                <Bell className="w-6 h-6 cursor-pointer text-primary-200 hover:text-primary-400" />
+                <span className="absolute top-0 right-0 w-2 h-2 bg-secondary-700 rounded-full"></span>
+              </div>
+
+              <DropdownMenu>
+                <DropdownMenuTrigger className="flex items-center gap-2 focus:outline-none">
+                  <Avatar>
+                    <AvatarImage src={authUser.userInfo?.image} />
+                    <AvatarFallback className="bg-primary-600">
+                      {authUser.userRole?.[0].toUpperCase()}
+                    </AvatarFallback>
+                  </Avatar>{" "}
+                  <p className="text-primary-200 hidden md:block">
+                    {authUser.userInfo?.name}
+                  </p>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent className="bg-white text-primary-700">
+                  <DropdownMenuItem
+                    className="cursor-pointer hover:!bg-primary-700 hover:!text-primary-100 font-bold"
+                    onClick={() =>
+                      router.push(
+                        authUser.userRole?.toLowerCase() === "manager"
+                          ? "/managers/properties"
+                          : "/tenants/favorites",
+                        { scroll: false }
+                      )
+                    }
+                  >
+                    Go to Dashboard
+                  </DropdownMenuItem>
+                  <DropdownMenuSeparator className="bg-primary-200" />
+                  <DropdownMenuItem
+                    className="cursor-pointer hover:!bg-primary-700 hover:!text-primary-100 "
+                    onClick={() =>
+                      router.push(
+                        `/${authUser.userRole?.toLowerCase()}s/settings`,
+                        { scroll: false }
+                      )
+                    }
+                  >
+                    Settings
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    className="cursor-pointer hover:!bg-primary-700 hover:!text-primary-100 "
+                    onClick={handleSignOut}
+                  >
+                    Sign out
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+            </>
+          ) : (
+            <>
+              <Link href="/signin">
+                <Button
+                  variant="outline"
+                  className="text-white border-white bg-transparent hover:bg-white hover:text-primary-700 rounded-lg "
+                >
+                  Sign In
+                </Button>
+              </Link>
+              <Link href="/signup">
+                <Button
+                  variant="secondary"
+                  className="text-white bg-secondary-600 hover:bg-white hover:text-primary-700 rounded-lg "
+                >
+                  Sign Up
+                </Button>
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
